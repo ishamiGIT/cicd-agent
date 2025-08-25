5b86d90305ecdc3ac4a7e6334e26142f1b193b40
Yeshwanth Gunasekaran <yesh@google.com>
2025-08-25 01:46:55 +0000

    Allow git branch and git tag for trigger creation

--- devops-mcp-server/cloud_build.py
+++ devops-mcp-server/cloud_build.py
@@ -2,10 +2,19 @@ import google.auth
 from googleapiclient import discovery
 from app import mcp
 import time
-
+from typing import Optional
 
 @mcp.tool
-def create_cloud_build_trigger(project_id: str, location_id: str, trigger_id: str, developer_connect_git_repository_link: str, service_account: str):
+def create_cloud_build_trigger(
+    project_id: str,
+    location_id: str,
+    trigger_id: str,
+    developer_connect_git_repository_link: str,
+    service_account: str,
+    *,  #  Makes subsequent arguments keyword-only
+    git_branch_push: Optional[str] = None,
+    git_tag_push: Optional[str] = None
+    ):
     """Creates a new Cloud Build trigger.
 
     Args:
@@ -13,22 +22,38 @@ def create_cloud_build_trigger(project_id: str, location_id: str, trigger_id: st
         location_id: The ID of the location for the trigger.
         trigger_id: The ID of the trigger to create.
         developer_connect_git_repository_link: The resource name of the Developer Connect GitRepositoryLink.
-        build_config_path: The path to the build configuration file.
+        git_branch_push (Optional[str]): The branch pattern to trigger on (e.g., "^main$").
+        git_tag_push (Optional[str]): The tag pattern to trigger on (e.g., "v*.*").
+    
+    Raises:
+        ValueError: If both or neither of git_branch_push and git_tag_push are provided.    
     """
     try:
+        if (git_branch_push and git_tag_push) or (not git_branch_push and not git_tag_push):
+            raise ValueError(
+                "Exactly one of 'git_branch_push' or 'git_tag_push' must be provided."
+            )
         credentials, project = google.auth.default(
             scopes=["https://www.googleapis.com/auth/cloud-platform"]
         )
         service = discovery.build('cloudbuild', 'v1', credentials=credentials)
 
         parent = f"projects/{project_id}/locations/{location_id}"
+        push_config = None
+        if git_branch_push:
+            push_config = {
+                "branch": git_branch_push,
+            }
+    
+        if git_tag_push:
+            push_config = {
+                "tag": git_tag_push,
+            }
         trigger = {
             "name": trigger_id,
             "developer_connect_event_config": {
                 "git_repository_link": developer_connect_git_repository_link,
-                "push": {
-                    "branch": "^main$",
-                },
+                "push": push_config,
             },
             "autodetect": True,
             "service_account" : service_account,
