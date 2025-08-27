0897275786762f67665fb1a20a58fad7ea931141
Yeshwanth Gunasekaran <yesh@google.com>
2025-08-27 15:14:08 +0000

    rename patterns

--- agent_root/cicd_agent/test.py
+++ agent_root/cicd_agent/test.py
@@ -1,6 +1,8 @@
+import vertexai
+from vertexai import rag
+
 RAG_PATTERNS_CORPUS_ID = "projects/haroonc-exp/locations/us-east4/ragCorpora/7205759403792793600"
 RAG_KNOWLEDGE_CORPUS_ID = "projects/haroonc-exp/locations/us-east4/ragCorpora/rag-cicd-knowledge"
-TARGET_FOLDER_PATH = os.environ.get('WORKING_DIR', '/data')
 vertexai.init(project="haroonc-exp", location="us-east4")
 
 rag_retrieval_config = rag.RagRetrievalConfig(
@@ -15,7 +17,8 @@ response = rag.retrieval_query(
             # rag_file_ids=["rag-file-1", "rag-file-2", ...],
         )
     ],
-    text=keywords,
+    text="time based",
     rag_retrieval_config=rag_retrieval_config,
 )
+print("Reponse: ")
 print(response)
